68c3f77f393837c992b8b6e7b0320311491e2ccd
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2019-11-28 17:22:10 +0100

    Hopefully works as intended. Last Byte is certainly wrong

--- Task2-AES/source_files/subbytes.v
+++ Task2-AES/source_files/subbytes.v
@@ -11,23 +11,20 @@ module subbytes(clk, rst, ena, state_in, state_out, done);
 	input [127:0] state_in;
 	output [127:0] state_out;
 	output done;
-
+	
+	// TODO: Implement the SubBytes AES operation
+	// ???
 	wire [7:0] sbox_out;
 	wire [7:0] sbox_in;
 
 	sbox sbox_inst(.byte_in(sbox_in), .byte_out(sbox_out));
 
-	parameter endCounter = 16;
+	parameter endIndex = 16;
 	reg[3:0] index;
 
 	parameter NOT_ENA=2'b00, INIT=2'b01, TRANSLATE=2'b10, LOOP_CONDITION=2'b11;
 	reg[1:0] state;
-
-
-	
-	// TODO: Implement the SubBytes AES operation
-	// ???
-	always @(posedge clk, posedge rst) begin
+	always @(posedge clk) begin
 		case (state) 
 			NOT_ENA: begin
 				done <= 'b0;
@@ -41,19 +38,23 @@ module subbytes(clk, rst, ena, state_in, state_out, done);
 				state <= TRANSLATE;
 			end
 			TRANSLATE: begin
-				sbox_in = state_in[index*8+8:index*8];
+				sbox_in = state_in[(index + 1) *8 - 1:index * 8];
 				state = LOOP_CONDITION;
 			end
 			LOOP_CONDITION: begin
-				if (index < endCounter) begin
-					state_out[index*8+8:index*8] = sbox_out;
+				state_out[(index + 1) * 8 - 1:index * 8] = sbox_out;
+				if (index < endIndex) begin
 					index++;
 					state <= TRANSLATE;
 				end
 				else begin
-					state <= INIT;
+					state_out = 128'd10;
+					done = 'b1;
+					state <= NOT_ENA;
 				end
 			end
+				default: state <= NOT_ENA;
+		endcase
 	end
 	
 endmodule

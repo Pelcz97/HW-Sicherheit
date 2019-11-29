aa5b4ea6ff6c129bc9e87041cabefe6400998fcc
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2019-11-29 14:36:14 +0100

    finally works as intended!

--- Task2-AES/source_files/shiftrows.v
+++ Task2-AES/source_files/shiftrows.v
@@ -15,22 +15,43 @@ module shiftrows(clk, rst, ena, state_in, state_out, done);
 	
 	// TODO: Implement the ShiftRows AES operation
 	// ???
-	reg state;
-	parameter NOT_ENA = 'b0, ENA = 'b1;
+	reg[1:0] state;
+	parameter NOT_ENA = 'b00, INIT = 'b01, TRANSLATE = 'b10, DONE = 'b11;
 
 	always@(posedge clk) begin
 		case (state)
 			NOT_ENA: begin
 				done <= 'b0;
 				if (ena) begin
-					state <= ENA;
+					state <= INIT;
 				end
 			end
-			ENA: begin
-				state_out[127:0] = 	{state_in[95:88], state_in[55:48], state_in[15:8], state_in[103:96],
-                        			state_in[63:56], state_in[23:16], state_in[111:104], state_in[71:64],
-                        			state_in[31:24], state_in[119:112], state_in[89:72], state_in[39:32],
-                        			state_in[127:120], state_in[87:80], state_in[47:40], state_in[7:0]};
+			INIT: begin
+				state_out <= 127'b0;
+				state <= TRANSLATE;
+			end
+			TRANSLATE: begin
+
+			state_out[7:0] <= state_in[7:0];
+			state_out[15:8] <= state_in[47:40];
+			state_out[23:16] <= state_in[87:80];
+			state_out[31:24] <= state_in[127:120];
+			state_out[39:32] <= state_in[39:32];
+			state_out[47:40] <= state_in[89:72];
+			state_out[55:48] <= state_in[119:112];
+			state_out[63:56] <= state_in[31:24];
+			state_out[71:64] <= state_in[71:64];
+			state_out[79:72] <= state_in[111:104];
+			state_out[87:80] <= state_in[23:16];
+			state_out[95:88] <= state_in[63:56];
+			state_out[127:120] <= state_in[95:88];
+			state_out[119:112] <= state_in[55:48];
+			state_out[111:104] <= state_in[15:8];
+			state_out[103:96] <= state_in[103:96];
+		
+			state <= DONE;
+			end
+			DONE: begin
 				done <= 'b1;
 				state <= NOT_ENA;
 			end

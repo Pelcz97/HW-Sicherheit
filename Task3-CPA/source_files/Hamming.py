86b4f0f367051725d74bb16fd8b1199cc6110354
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2019-12-30 10:53:21 +0100

    Added a file for Hamming Stuf

---
+++ Task3-CPA/source_files/Hamming.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def HammingWeightXOR(string1, string2):
+
+
+def HammingDistance(string):
+    result = 0
+    for i in range(len(string)):
+        if (string[i]) != 0
+            result += 1
+    
+return result
+
+numTraces = 2000

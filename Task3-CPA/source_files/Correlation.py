3a84fea078b01f1d98ef257962ea0cb8e945d149
Jan-Luca <janlucavettel@gmx.de>
2020-01-01 15:59:31 +0100

    Format changes

--- Task3-CPA/source_files/Correlation.py
+++ Task3-CPA/source_files/Correlation.py
@@ -6,100 +6,47 @@ import ctypes
 
 class Correlation:
 
-# Playground for parallel stuff
-#shared_array_base = mp.Array(ctypes.c_double, 256*87)
-#shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
-#shared_array = shared_array.reshape(256, 87)
-
-    # shared_r_base = mp.Array(ctypes.c_float, 256*87)
-    # shared_r = np.ctypeslib.as_array(shared_r_base.get_obj())
-    # shared_r = shared_r.reshape(256, 87)
-
-def __init__(self, H, T):
-    self.H = H
-    self.T = T
-    self.mean_h = np.mean(H, axis=0)
-    self.mean_t = np.mean(T, axis=0)
-    self.len_T = len(T)
-    self.len_T0 = len(T[0])
-    self.len_H0 = len(H[0])
-
-# Parallel processing
-def my_func(self, i):
-    shared_array[i, :] = i
+    def __init__(self, H, T):
+        self.H = H
+        self.T = T
+        self.mean_h = np.mean(H, axis=0)
+        self.mean_t = np.mean(T, axis=0)
+        self.len_T = len(T)
+        self.len_T0 = len(T[0])
+        self.len_H0 = len(H[0])
 
 
-#pool = mp.Pool(mp.cpu_count())
-#pool.map(my_func, range(256))
+    # Even faster correlation trace computation
+    # Takes the full matrix of predictions instead of just a column
+    # O - (n,t) array of n traces with t samples each
+    # P - (n,m) array of n predictions for each of the m candidates
+    # returns an (m,t) correaltion matrix of m traces t samples each
+    def correlationTraces(self, O, P):
+        (n, t) = O.shape  # n traces of t samples
+        (n_bis, m) = P.shape  # n predictions for each of m candidates
 
-#print(shared_array)
+        DO = O - (np.einsum("nt->t", O, dtype='float64', optimize='optimal') / np.double(n))  # compute O - mean(O)
+        DP = P - (np.einsum("nm->m", P, dtype='float64', optimize='optimal') / np.double(n))  # compute P - mean(P)
 
-#pool.close()
+        numerator = np.einsum("nm,nt->mt", DP, DO, optimize='optimal')
+        tmp1 = np.einsum("nm,nm->m", DP, DP, optimize='optimal')
+        tmp2 = np.einsum("nt,nt->t", DO, DO, optimize='optimal')
+        tmp = np.einsum("m,t->mt", tmp1, tmp2, optimize='optimal')
+        denominator = np.sqrt(tmp)
 
-###########################################
-
-# Even faster correlation trace computation
-# Takes the full matrix of predictions instead of just a column
-# O - (n,t) array of n traces with t samples each
-# P - (n,m) array of n predictions for each of the m candidates
-# returns an (m,t) correaltion matrix of m traces t samples each
-def correlationTraces(self, O, P):
-    (n, t) = O.shape  # n traces of t samples
-    (n_bis, m) = P.shape  # n predictions for each of m candidates
-
-    DO = O - (np.einsum("nt->t", O, dtype='float64', optimize='optimal') / np.double(n))  # compute O - mean(O)
-    DP = P - (np.einsum("nm->m", P, dtype='float64', optimize='optimal') / np.double(n))  # compute P - mean(P)
-
-    numerator = np.einsum("nm,nt->mt", DP, DO, optimize='optimal')
-    tmp1 = np.einsum("nm,nm->m", DP, DP, optimize='optimal')
-    tmp2 = np.einsum("nt,nt->t", DO, DO, optimize='optimal')
-    tmp = np.einsum("m,t->mt", tmp1, tmp2, optimize='optimal')
-    denominator = np.sqrt(tmp)
-
-    return numerator / denominator
+        return numerator / denominator
 
 
-def rowOfAttack(self, i):
+    def rowOfAttack(self, i):
 
-    print("New Row")
-    print("Shared_LenT0: ", len_T0)
-    for j in range(len_T0):
-        print("row {0}, element {1}".format(i, j))
-        numerator = 0
-        sum1 = 0
-        sum2 = 0
-        for d in range(len_T):
-            mul1 = H[d][i] - mean_h[i]
-            mul2 = T[d][j] - mean_t[j]
-            mul = np.multiply(mul1, mul2)
-            numerator += mul
-
-            sum1 += np.square(H[d][i] - mean_h[i])
-            sum2 += np.square(T[d][j] - mean_t[j])
-
-        denominator = np.multiply(sum1, sum2)
-        denominator = np.sqrt(denominator)
-        shared_r[j][i] = np.divide(numerator, denominator)
-
-
-def attackingWithCorrelation(self, H, T):
-
-    start = time.time()
-
-    endMean = time.time()
-
-    pool = mp.Pool(mp.cpu_count())
-
-    print(endMean - start)
-    print("Shared len H0 ", H)
-    pool.map(rowOfAttack, range(len_H0))
-
-    '''for i in range(len_H0):
+        print("New Row")
+        print("Shared_LenT0: ", len_T0)
         for j in range(len_T0):
+            print("row {0}, element {1}".format(i, j))
             numerator = 0
             sum1 = 0
             sum2 = 0
-            for d in range(len(T)):
+            for d in range(len_T):
                 mul1 = H[d][i] - mean_h[i]
                 mul2 = T[d][j] - mean_t[j]
                 mul = np.multiply(mul1, mul2)
@@ -110,11 +57,42 @@ def attackingWithCorrelation(self, H, T):
 
             denominator = np.multiply(sum1, sum2)
             denominator = np.sqrt(denominator)
-            r[j][i] = np.divide(numerator, denominator)
+            shared_r[j][i] = np.divide(numerator, denominator)
 
-            print("Step i:{0}, j:{1}".format(i, j))'''
-    pool.close()
-    end = time.time()
-    print(end - start)
-    return shared_r
+
+    def attackingWithCorrelation(self, H, T):
+
+        start = time.time()
+
+        endMean = time.time()
+
+        pool = mp.Pool(mp.cpu_count())
+
+        print(endMean - start)
+        print("Shared len H0 ", H)
+        pool.map(rowOfAttack, range(len_H0))
+
+        '''for i in range(len_H0):
+            for j in range(len_T0):
+                numerator = 0
+                sum1 = 0
+                sum2 = 0
+                for d in range(len(T)):
+                    mul1 = H[d][i] - mean_h[i]
+                    mul2 = T[d][j] - mean_t[j]
+                    mul = np.multiply(mul1, mul2)
+                    numerator += mul
+    
+                    sum1 += np.square(H[d][i] - mean_h[i])
+                    sum2 += np.square(T[d][j] - mean_t[j])
+    
+                denominator = np.multiply(sum1, sum2)
+                denominator = np.sqrt(denominator)
+                r[j][i] = np.divide(numerator, denominator)
+    
+                print("Step i:{0}, j:{1}".format(i, j))'''
+        pool.close()
+        end = time.time()
+        print(end - start)
+        return shared_r
 

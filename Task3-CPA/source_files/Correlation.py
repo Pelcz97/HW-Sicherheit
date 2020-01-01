1c618412ed82fa5e49d20c46e657dc9115c4474c
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2020-01-01 15:52:23 +0100

    made the correlation file to a class

--- Task3-CPA/source_files/Correlation.py
+++ Task3-CPA/source_files/Correlation.py
@@ -4,26 +4,28 @@ import multiprocessing as mp
 import ctypes
 
 
+class Correlation:
+
 # Playground for parallel stuff
 #shared_array_base = mp.Array(ctypes.c_double, 256*87)
 #shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
 #shared_array = shared_array.reshape(256, 87)
 
-shared_r_base = mp.Array(ctypes.c_float, 256*87)
-shared_r = np.ctypeslib.as_array(shared_r_base.get_obj())
-shared_r = shared_r.reshape(256, 87)
+    shared_r_base = mp.Array(ctypes.c_float, 256*87)
+    shared_r = np.ctypeslib.as_array(shared_r_base.get_obj())
+    shared_r = shared_r.reshape(256, 87)
 
-shared_H = np.zeros((6000, 87))
-shared_T = np.zeros((6000, 256))
-shared_mean_H = np.zeros(87)
-shared_mean_T = np.zeros(256)
-
-shared_len_T = 0
-shared_lenT0 = 0
-shared_lenH0 = 0
+def __init__(self, H, T):
+    self.H = H
+    self.T = T
+    self.mean_h = np.mean(H, axis=0)
+    self.mean_t = np.mean(T, axis=0)
+    self.len_T = len(T)
+    self.len_T0 = len(T[0])
+    self.len_H0 = len(H[0])
 
 # Parallel processing
-def my_func(i):
+def my_func(self, i):
     shared_array[i, :] = i
 
 
@@ -41,7 +43,7 @@ def my_func(i):
 # O - (n,t) array of n traces with t samples each
 # P - (n,m) array of n predictions for each of the m candidates
 # returns an (m,t) correaltion matrix of m traces t samples each
-def correlationTraces(O, P):
+def correlationTraces(self, O, P):
     (n, t) = O.shape  # n traces of t samples
     (n_bis, m) = P.shape  # n predictions for each of m candidates
 
@@ -57,49 +59,40 @@ def correlationTraces(O, P):
     return numerator / denominator
 
 
-def rowOfAttack(i):
+def rowOfAttack(self, i):
 
     print("New Row")
-    print("Shared_LenT0: ", shared_lenT0)
-    for j in range(shared_lenT0):
+    print("Shared_LenT0: ", len_T0)
+    for j in range(len_T0):
         print("row {0}, element {1}".format(i, j))
         numerator = 0
         sum1 = 0
         sum2 = 0
-        for d in range(shared_len_T):
-            mul1 = shared_H[d][i] - shared_mean_H[i]
-            mul2 = shared_T[d][j] - shared_mean_T[j]
+        for d in range(len_T):
+            mul1 = H[d][i] - mean_h[i]
+            mul2 = T[d][j] - mean_t[j]
             mul = np.multiply(mul1, mul2)
             numerator += mul
 
-            sum1 += np.square(shared_H[d][i] - shared_mean_H[i])
-            sum2 += np.square(shared_T[d][j] - shared_mean_T[j])
+            sum1 += np.square(H[d][i] - mean_h[i])
+            sum2 += np.square(T[d][j] - mean_t[j])
 
         denominator = np.multiply(sum1, sum2)
         denominator = np.sqrt(denominator)
         shared_r[j][i] = np.divide(numerator, denominator)
 
 
-def attackingWithCorrelation(H, T):
-
-    shared_H = H
-    shared_T = T
+def attackingWithCorrelation(self, H, T):
 
     start = time.time()
-    shared_mean_H = np.mean(H, axis=0)
-    shared_mean_T = np.mean(T, axis=0)
-    endMean = time.time()
 
-    shared_len_T = len(T)
-    shared_lenT0 = len(T[0])
-    print("ORIGIN T0: ", len(T[0]))
-    shared_lenH0 = len(H[0])
+    endMean = time.time()
 
     pool = mp.Pool(mp.cpu_count())
 
     print(endMean - start)
-    print("Shared len H0 ", shared_lenH0)
-    pool.map(rowOfAttack, range(shared_lenH0))
+    print("Shared len H0 ", H)
+    pool.map(rowOfAttack, range(len_H0))
 
     '''for i in range(len_H0):
         for j in range(len_T0):

f7e92ecfc2be2cddd2c8344353ecb7f38312119e
Philipp Pelcz <philipp.pelcz@student.kit.edu>
2020-01-13 10:35:03 +0100

    Now every Byte and every Bit is getting checked in parrallel. The Images are stored in the rootDir

--- Task3-CPA/source_files/CPA.py
+++ Task3-CPA/source_files/CPA.py
@@ -10,77 +10,108 @@ from sys import platform
 from Correlation import Correlation
 from Crypto.Cipher import AES
 import binascii
+import time
+import multiprocessing as mp
+import itertools
 
-if platform == "linux" or platform == "linux2":
-    TRACES = '/home/philipp/workspace/hw-security-course-ws19/Task3-CPA/example_traces/test_traces.csv'
-    MSGS = '/home/philipp/workspace/hw-security-course-ws19/Task3-CPA/example_traces/test_msgs.csv'
-elif platform == "darwin":
-    TRACES = '/Users/janlucavettel/Documents/FPGA/HW-Sicherheit/Task3-CPA/example_traces/test_traces.csv'
-    MSGS = '/Users/janlucavettel/Documents/FPGA/HW-Sicherheit/Task3-CPA/example_traces/test_msgs.csv'
-#elif platform == "win32":
-    # Windows...
-
-
-traces = genfromtxt(TRACES, delimiter=',')
-
-with open(MSGS, newline='') as csvfile:
-    msgs = list(csv.reader(csvfile))
+def CPA(indices):
+    if platform == "linux" or platform == "linux2":
+        TRACES = '/home/philipp/workspace/hw-security-course-ws19/Task3-CPA/source_files/traces.csv'
+        MSGS = '/home/philipp/workspace/hw-security-course-ws19/Task3-CPA/source_files/messages.csv'
+    elif platform == "darwin":
+        TRACES = '/Users/janlucavettel/Documents/FPGA/HW-Sicherheit/Task3-CPA/example_traces/test_traces.csv'
+        MSGS = '/Users/janlucavettel/Documents/FPGA/HW-Sicherheit/Task3-CPA/example_traces/test_msgs.csv'
+    #elif platform == "win32":
+        # Windows...
 
 
-key = msgs[0][0]
-key = binascii.unhexlify(key)
-cipher = AES.new(key, AES.MODE_ECB)
-decipher = AES.new(key, AES.MODE_ECB)
-ciphertexts = []
+    traces = genfromtxt(TRACES, delimiter=',')
 
-for i in range(len(msgs)):
-    text = cipher.encrypt(binascii.unhexlify(msgs[i][1]))
-    a = binascii.hexlify(text).lower()
-    a = a.decode("utf-8")
-    msgs[i].append(a)
+    with open(MSGS, newline='') as csvfile:
+        msgs = list(csv.reader(csvfile))
 
 
+    key = msgs[0][0]
+    key = binascii.unhexlify(key)
+    cipher = AES.new(key, AES.MODE_ECB)
+    decipher = AES.new(key, AES.MODE_ECB)
+    ciphertexts = []
 
-msgs = np.array(msgs, dtype=str)
-
-
-#Set this to the Byte you want to attack
-numByte = 0
-numBit = 0
-
-numTraces = traces.shape[0]
-traceLength = traces.shape[1]
-
-k = np.arange(0, 256)
-H = np.zeros((256, len(msgs)))
+    # for i in range(len(msgs)):
+    #     text = cipher.encrypt(binascii.unhexlify(msgs[i][1]))
+    #     a = binascii.hexlify(text).lower()
+    #     a = a.decode("utf-8")
+    #     msgs[i].append(a)
 
 
-for i in range(len(k)):
-    for j in range(len(msgs)):
-        msg = msgs[j, 2]
-        msg = msg[2*numByte:2*numByte+2]
-        msg = int(msg, 16)
-        H[i,j] = getInvSboxValue(msg ^ k[i])
-        H[i,j] = np.bitwise_and(np.array(H[i,j]).astype(int), 2**numBit)
 
-HModel = H
-for i in range(len(H)):
-    HModel[i] = np.array(list(map(Hamming.HammingDistanceInt, H[i])))
-
-HModel = HModel.T
-
-correlationObject = Correlation(HModel,traces)
-corrMatrix = correlationObject.correlationTraces(traces, HModel)
-
-maxValue = np.amax(np.abs(corrMatrix))
-result = np.where(np.abs(corrMatrix) == maxValue)
+    # msgs = np.array(msgs, dtype=str)
 
 
-plt.plot(corrMatrix.T, color='gray')
-plt.plot(corrMatrix[result[0]].T, color='red')
-plt.show()
+    #Set this to the Byte you want to attack
+    print("Running ", indices)
+    numByte = indices[0]
+    numBit = indices[1]
 
-print("Max value is: ", maxValue)
+    numTraces = traces.shape[0]
+    traceLength = traces.shape[1]
 
-print("Number of traces: ", numTraces)
-print("Trace length: ", traceLength)
+
+    k = np.arange(0, 256)
+    H = np.zeros((256, len(msgs)))
+
+    start = time.time()
+
+    for i in range(len(k)):
+        for j in range(len(msgs)):
+            msg = msgs[j][2]
+            msg = msg[2*numByte:2*numByte+2]
+            msg = int(msg, 16)
+            H[i,j] = getInvSboxValue(msg ^ k[i])
+            H[i,j] = np.bitwise_and(np.array(H[i,j]).astype(int), 2**numBit)
+
+    HModel = H
+    for i in range(len(H)):
+        HModel[i] = np.array(list(map(Hamming.HammingDistanceInt, H[i])))
+
+    endPower = time.time()
+
+    HModel = HModel.T
+
+    correlationObject = Correlation(HModel,traces)
+    corrMatrix = correlationObject.correlationTraces(traces, HModel)
+
+    endCorr = time.time()
+
+    maxValue = np.amax(np.abs(corrMatrix))
+    result = np.where(np.abs(corrMatrix) == maxValue)
+
+
+    filename = str(numByte) + "_" + str(numBit)
+    plt.plot(corrMatrix.T, color='gray')
+    plt.plot(corrMatrix[result[0]].T, color='red')
+    title = "BYTE_BIT: " + filename + "KEYHYP: " + str(result[0]) + "TRACE MOMENT: " + str(result[1])
+    plt.title(title)
+    filename = filename + ".png"
+    plt.savefig(filename, dpi=300)
+
+    print("------------------------------------------")
+    print("Byte number is: ", numByte)
+    print("Bit number is: ", numBit)
+    print("Number of traces: ", numTraces)
+    print("Trace length: ", traceLength)
+    print("Power took", endPower - start)
+    print("Correlation took", endCorr - start)
+    print("Max value is: ", maxValue)
+    print("The best Key Hyp is: ", result[0])
+    print("Korrelation is in point: ", result[1])
+    print("------------------------------------------")
+
+
+a = range(16)
+b = range(8)
+
+allCombinations = list(itertools.product(a,b))
+
+with mp.Pool(processes=8) as pool:
+    pool.map(CPA, allCombinations)
